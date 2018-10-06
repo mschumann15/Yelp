75a030825073529d575e8b028a2a3a723bdb8ecc
Marcus <marcus.eshs@gmail.com>
2018-10-05 19:56:32 -0700

    Commit2

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,26 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Marcus Schumann on 10/5/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
